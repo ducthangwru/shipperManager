ce1861ceedf5ab04fe5025d544e28d7889a13700
DucThangWRU <ducthangwru97@gmail.com>
2018-03-15 14:13:47 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const configPort = process.env.PORT || 3000;
 const config = require('./config.json');
+const handlebars = require('express-handlebars');
 
 const userApi = require('./modules/api/users/usersController');
 const groupApi = require('./modules/api/groups/groupsController');
@@ -12,10 +13,14 @@ var app = express();
 
 app.use(bodyParser.json({ extended : true}));
 app.use(bodyParser.urlencoded({ extended : true}));
+// app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
 
 app.use('/api/user', userApi);
 app.use('/api/group', groupApi);
 app.use('/api/order', orderApi);
+
+app.use(express.static(__dirname + '/public'));
 
 mongoose.connect(config.connectionString, (err) => {
   if (err) {
